9210de70155804cfcd58b6959d14040f2459c800
Nicolas Silva <araujo.silva.nicolas@gmail.com>
2025-06-08 16:46:04 -0300

    Viewsets created(need to check get_queryset for return on type Never)

--- core/views.py
+++ core/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Padaria, Fornada, Inscricao
+from .serializers import PadariaSerializer, FornadaSerializer, InscricaoSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+
+
+class PadariaViewSet(viewsets.ModelViewSet):
+    queryset = Padaria.objects.all()
+    serializer_class = PadariaSerializer
+
+class FornadaViewSet(viewsets.ModelViewSet):
+    queryset = Fornada.objects.all()
+    serializer_class = FornadaSerializer
+
+class InscricaoViewSet(viewsets.ModelViewSet):
+    queryset = Inscricao.objects.all()
+    serializer_class = InscricaoSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Inscricao.objects.filter(usuario=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(usuario=self.request.user)
